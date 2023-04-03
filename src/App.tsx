b4a72ad2e4fbc486dbda0c1e20cc78752be1b84c
maxchu0523 <47725098+maxchu0523@users.noreply.github.com>
2023-04-03 16:02:54 -0400

    Implmented about me page and tailwind css components

--- src/App.tsx
+++ src/App.tsx
@@ -41,25 +41,25 @@ function App() {
           <div className='px-4 py-4 md:px-28 md:py-24' >
 
 
-            <div className='text-4xl md:text-7xl subpixel-antialiased font-mono text-left py-8'>
+            <div className='headerText text-left py-8'>
               <div className='text-white'>Web Developer,</div>
               <div className='text-orange-500'>Max</div>
             </div>
-            <div className='text-base text-white subpixel-antialiased font-mono text-left  py-4'>
+            <div className='contentText text-left  py-4'>
               <p>I am a Full-Stack Web Developer. I code Website, Hybird app and ERP System</p>
 
               <p>Born in late 90s, Raised in Hong Kong, Living in Toronto</p>
             </div>
 
             <div className='flex flex-row flex-wrap text-white subpixel-antialiased font-mono gap-4'>
-              {/* <div className='min-w-24 border-2 my-1 px-3 cursor-pointer hover:border-orange-500' onClick={scrollToAboutMe}>About</div> */}
-              <div className='min-w-24 border-2 my-1 px-3 cursor-pointer hover:border-orange-500' onClick={scrollToProject}>Work Experience</div>
+              <div className='min-w-24 border-2 my-1 px-3 cursor-pointer hover:border-orange-500' onClick={scrollToAboutMe}>About</div>
+              <div className='min-w-24 border-2 my-1 px-3 cursor-pointer hover:border-orange-500' onClick={scrollToProject}>Work</div>
               <div className='min-w-24 border-2 my-1 px-3 cursor-pointer hover:border-orange-500' onClick={scrollToContact}>Contact</div>
             </div>
 
             <div className='flex flex-row flex-wrap text-white subpixel-antialiased font-mono gap-5 py-4 y-4'>
-              <FontAwesomeIcon icon={faLinkedinIn} onClick={redirectTo('https://www.linkedin.com/in/max-chu-06b944181/')} className='cursor-pointer' />
-              <FontAwesomeIcon icon={faGithub} onClick={redirectTo('https://github.com/maxchu0523')} className='cursor-pointer' />
+              <FontAwesomeIcon icon={faLinkedinIn} onClick={redirectTo('https://www.linkedin.com/in/max-chu-06b944181/')} className='cursor-pointer hover:scale-150 hover:rotate-12' />
+              <FontAwesomeIcon icon={faGithub} onClick={redirectTo('https://github.com/maxchu0523')} className='cursor-pointer hover:scale-150 hover:rotate-12' />
             </div>
 
           </div>
@@ -71,19 +71,51 @@ function App() {
           </div>
         </div>
 
-        <div className='min-h-screen bg-gray-900 py-42' ref={projectRef} >
+        <div className='min-h-screen bg-gray-900 py-42' ref={aboutMeRef} >
+          <div className='headerText text-center pb-4 pt-12'>
+            <p className='text-white'>About Me</p>
+          </div>
+          <div className='contentText text-left py-8'>
+            <div className='px-4 py-4 md:px-28 md:py-24'>
+              <p>My name is Yat-Long Chu, you can also call me Max. I am a full-stack web developer in Toronto. As a full-stack developer, I get involved in all aspects of the development process and work with various languages and tools. It keeps me engaged and constantly learning. I also have a passion for creating intuitive UX. It brings me a sense of accomplishment when seeing users enjoy interacting with my work</p>
 
-          <div className='text-4xl md:text-7xl subpixel-antialiased font-mono text-center py-4'>
-            <p className='text-white'>Work Experience</p>
+              
+            </div>
           </div>
 
 
-          <div className='md:flex flex-wrap flex-row justify-around subpixel-antialiased font-mono py-4 content-center'>
+          <div className='contentText bold px-4 md:px-28 underline underline-offset-2'>
+            <p>Tool</p>
+          </div>
+          <div className='contentText text-left'>
+            <div className='px-4 md:px-28'>
+              <div className='box=content  flex gap-4 flex-wrap'>
+                <div className='bg-gray-600'>React</div>
+                <div className='bg-gray-600'>Angular</div>
+                <div className='bg-gray-600'>NextJs</div>
+                <div className='bg-gray-600'>Ionic</div>
+                <div className='bg-gray-600'>Php</div>
+                <div className='bg-gray-600'>Java</div>
+                <div className='bg-gray-600'>C#</div>
+              </div>
+            </div>
+          </div>
 
-            <div className='basis-1/3 p-8'>
+        </div>
+
+        <div className='min-h-screen bg-gray-900 py-42' ref={projectRef} >
+
+          <div className='headerText text-white text-center py-12'>
+            <p>Work Experience</p>
+          </div>
+
+
+          <div className='md:flex flex-wrap flex-row justify-around subpixel-antialiased font-mono content-center pl-8 pr-8'>
+
+            <div className='basis-1/3 pb-8'>
               <img className='h-48 rounded-md  w-max my-4' src={myPodium} onClick={redirectTo('https://www.oocllogistics.com/eng/ourservices/eservices/podium/Pages/default.aspx')}></img>
-              <p className='text-white font-bold text-left'>My Podium</p>
-              <div className='text-base text-white subpixel-antialiased font-mono text-left'>
+              <p className='contentText font-bold text-left'>My Podium</p>
+              <div className='contentText text-left'>
                 <p>Module-based International Supply Chain Management IT platform</p>
               </div>
             </div>
@@ -91,19 +123,19 @@ function App() {
 
 
 
-            <div className='basis-1/3 p-8'>
+            <div className='basis-1/3 pb-8'>
               <img className='h-48 rounded-md  w-max my-4' src={actnow} onClick={redirectTo('https://actnowmh.com/')}></img>
-              <p className='text-white font-bold text-left'>Missionary Holiday</p>
-              <div className='text-base text-white subpixel-antialiased font-mono text-left'>
+              <p className='contentText font-bold text-left'>Missionary Holiday</p>
+              <div className='contentText text-left'>
                 <p>Grasp all the information related to christian mission trips, offering trips ranging from two months to two years</p>
               </div>
             </div>
 
 
-            <div className='basis-1/3 p-8'>
+            <div className='basis-1/3 pb-8'>
               <img className='h-48 rounded-md  w-max my-4' src={harilelaVip} onClick={redirectTo('https://apps.apple.com/us/app/harilela-vip/id1571846151')}></img>
-              <p className='text-white font-bold text-left'>Harilela VIP </p>
-              <div className='text-base text-white subpixel-antialiased font-mono text-left'>
+              <p className='contentText font-bold text-left'>Harilela VIP </p>
+              <div className='contentText text-left'>
                 <p>A private VIP App for friends and family of the Harilela Family to enjoy benefits and discounts at the Harilela Hotels in Hong Kong</p>
               </div>
             </div>
@@ -115,23 +147,21 @@ function App() {
 
 
         <div className='min-h-screen bg-gray-900 py-42' ref={contactRef} >
-          <div className='my-60'>
-            <div className='text-4xl md:text-7xl subpixel-antialiased font-mono text-center py-4'>
-              <p className='text-white'>Get In Touch</p>
-            </div>
-            <div className='text-base text-white subpixel-antialiased font-mono text-center  py-4'>
-              <p>Let's have a chat and a decent cup of coffee.</p>
-            </div>
+          <div className='headerText text-center pb-4 pt-48'>
+            <p >Get In Touch</p>
+          </div>
+          <div className='contentText text-center  py-4'>
+            <p>Let's have a chat and a decent cup of coffee.</p>
+          </div>
 
-            <div className='py-8'>
-              <a className="bg-transparent hover:bg-orange-500 text-white font-semibold hover:text-white py-4 px-4 border border-orange-500 hover:border-transparent rounded-bl-2xl rounded-tr-2xl text-base subpixel-antialiased font-mono text-center  py-4" href="mailto:maxchu0523@gmail.com">
-                Let's Go
-              </a>
-            </div>
-            <div className='flex flex-row flex-wrap text-white subpixel-antialiased font-mono gap-5 py-4 y-4 justify-center'>
-              <FontAwesomeIcon icon={faLinkedinIn} onClick={redirectTo('https://www.linkedin.com/in/max-chu-06b944181/')} className='cursor-pointer' />
-              <FontAwesomeIcon icon={faGithub} onClick={redirectTo('https://github.com/maxchu0523')} className='cursor-pointer' />
-            </div>
+          <div className='py-8 flex justify-center'>
+            <a className="bg-transparent hover:bg-orange-500 text-white font-semibold hover:text-white py-4 px-4 border border-orange-500 hover:border-transparent rounded-bl-2xl rounded-tr-2xl text-base subpixel-antialiased font-mono text-center  py-4" href="mailto:maxchu0523@gmail.com">
+              Let's Go
+            </a>
+          </div>
+          <div className='flex flex-row flex-wrap text-white subpixel-antialiased font-mono gap-5 py-4 y-4 justify-center'>
+            <FontAwesomeIcon icon={faLinkedinIn} onClick={redirectTo('https://www.linkedin.com/in/max-chu-06b944181/')} className='cursor-pointer hover:scale-150 hover:rotate-12' />
+            <FontAwesomeIcon icon={faGithub} onClick={redirectTo('https://github.com/maxchu0523')} className='cursor-pointer hover:scale-150 hover:rotate-12' />
           </div>
 
 
